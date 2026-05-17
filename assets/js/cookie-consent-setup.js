6f4b31eb154ee37fd637c1b5c3fa784233c1e32a
JinuG6328 <harrisan21@hanmail.net>
2026-05-17 22:57:33 +0000

    Deploying to gh-pages from @ JinuG6328/jinug6328-dev2@2ec00fb7523a4c4de9c6967323d258119703c753 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://alshedivat.github.io/al-folio/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://jinug6328.github.io/jinug6328-dev2/#contact">contact us</a>.'
               }
             ]
           }
